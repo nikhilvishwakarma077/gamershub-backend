87e75ef8ce02ae2c03a38754b70d477480cb99f7
nikhilvishwakarma077 <nikhilvishwakarma7707@gmail.com>
2026-06-07 18:03:54 +0530

    fixed

--- src/app.ts
+++ src/app.ts
@@ -16,15 +16,14 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(
-    cors({
-        origin: [
-            "http://localhost:5173",
-            "https://gamershub-frontend-two.vercel.app",
-        ],
-        credentials: true,
-    })
-);
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://gamershub-frontend-two.vercel.app"
+  ],
+  credentials: true
+}));
 
 connectDB()
 
