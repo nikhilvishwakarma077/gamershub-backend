9c5a2032623b7ad0ca60664cb7cf798726fdb2da
nikhilvishwakarma077 <nikhilvishwakarma7707@gmail.com>
2026-06-19 19:11:56 +0530

    updated domain

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://gamershub-frontend-two.vercel.app"
+    "https://gamershub-ffm.vercel.app"
   ],
   credentials: true
 }));
